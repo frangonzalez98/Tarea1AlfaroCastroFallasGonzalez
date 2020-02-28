9d4ae753c60bea4887bf19227c8a6e1e6693c4fa
ALFARO CORDERO RANDALL ELIECER <ra.kamuk>
2020-02-28 12:41:05 -0600

    Arreglos del codigo con errores para Flake 8

--- Flake8Fails.py
+++ Flake8Fails.py
@@ -16,62 +16,28 @@
 # para cada caso.
 
 import random
+import math
 
 
-# PyFlakes puede detectar cuando una variable
-# no esta siendo usada.
-# El error reportado debe iniciar con una F
+# Se removio la variable sin usar
 def flakes_unused_variable(a):
-    sinUsar = 5
     usada = 10
     return usada+a
 
-# Aquí se producirá otro error por problemas de
-# formato según PEP8.
-# El plugin pycodestyle interviene, y el error
-# detectado inicia con E o W.
-# En este caso particular, el def debió tener
-# dos espacios vacíos en la parte superior
-# pero solo tiene uno (antes del comentario)
+
+# Se agrego la linea faltante
 def useful_function(a, b):
     return a+b
 
 
-# Aqui hay otro error adicional, debido a que esta linea es muy larga y por lo tanto estorba en la lectura del código.
-# Esta funcion es innecesariamente complicada
-# McCabe incorporado en Flake8 lo detecta
+# Ya no hay una linea excesivamente larga
+# y se arreglo al complejidad de la funcion
 def funcion_atroz():
-    # Bien se conoce que el uso excesivo de elif no es bienvenido
-    # Note entonces como McCabe detecta este error
     randNumber = random.random()
-    if randNumber <= 0.1:
-        return 1
-    elif randNumber <= 0.2:
-        return 2
-    elif randNumber <= 0.3:
-        return 3
-    elif randNumber <= 0.4:
-        return 4
-    elif randNumber <= 0.5:
-        return 5
-    elif randNumber <= 0.6:
-        return 6
-    elif randNumber <= 0.7:
-        return 7
-    elif randNumber <= 0.8:
-        return 8
-    elif randNumber <= 0.9:
-        return 9
-    elif randNumber <= 10:
-        return 10
+    return math.ceil(randNumber*10)
 
 
-# Ya que el error anterior no se nota si no se especifica la
-# complejidad de McCabe deseada, entonces se presenta un error
-# alternativo final
-# Si se desea detectar, utilice --max-complexity 10
-
-
-# Otro error es el de una variable inexistente
+# Se creó la variable
 def not_found_variable():
+    variableInexistente = 4
     return variableInexistente
